d768f4f154d5ca21cec4162847ea6be0fa552549
Alexandrebelin <belin.alexandre.77@gmail.com>
2021-04-23 21:20:29 +0200

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,12 +2,13 @@ import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
+    <footer className={styles.footerTop}>
       2021 © Made with&nbsp;<strong>React.js</strong>&nbsp; by&nbsp;
       <a
         href="https://github.com/Alexandrebelin"
         rel="noopener noreferrer"
         target="_blank"
+        className={styles.footerTxt}
       >
         Alexandre Belin
       </a>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import "../styles/globals.css";
+import styles from "../styles/Footer.module.css";
 
 import { CookiesProvider } from "react-cookie";
 import Footer from "../components/Footer";
@@ -7,9 +8,11 @@ import Footer from "../components/Footer";
 function MyApp({ Component, pageProps }) {
   return (
     <CookiesProvider>
-      <Header></Header>
-      <Component {...pageProps} />
-      <Footer />
+      <div className={styles.mainContainer}>
+        <Header></Header>
+        <Component {...pageProps} />
+        <Footer />
+      </div>
     </CookiesProvider>
   );
 }
